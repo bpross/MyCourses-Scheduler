2f04383aa55887e9303bdd53a4bce37dd67aa134
whimsy <whimsy@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-08 00:53:24 +0000

    Server side changes. Not done yet.
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@148 24d622c1-b139-487c-8c47-8fc189a5de26

--- django/scheduler/urls.py
+++ django/scheduler/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from django_consultants.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,7 +13,7 @@ urlpatterns = patterns('',
 
 	# Login / logout
     (r'^login/$', 'django.contrib.auth.views.login'),
-	(r'^logout/$', logout_page),
+	(r'^logout/$', 'scheduler.views.logout'),
 	# Web portal
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
