6557bb4ffd5d6606ce3c7271c20703be68bd80b1
whimsy <whimsy@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-08 00:43:01 +0000

    Test changes to views and urls
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@146 24d622c1-b139-487c-8c47-8fc189a5de26

--- django/scheduler/urls.py
+++ django/scheduler/urls.py
@@ -7,8 +7,14 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^scheduler/', include('scheduler.foo.urls')),
+
+	# Interface
     (r'^$', 'scheduler.views.index'),
-    (r'^login/$', 'scheduler.views.login'),
+
+	# Login / logout
+    (r'^login/$', 'django.contrib.auth.views.login'),
+	(r'^logout/$', logout_page),
+	# Web portal
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
--- django/scheduler/views.py
+++ django/scheduler/views.py
@@ -1,4 +1,6 @@
 # Create your views here.
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
 def index(request):
@@ -6,3 +8,10 @@ def index(request):
 
 def login(request):
     return render_to_response('login.html')
+
+def logout(request):
+	"""
+	Log users out and redirect them to the main page.
+	"""
+	logout(request)
+	return HttpResponseRedirect('/')
