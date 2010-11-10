799066cb3be1e6a6a34524c18dda2596790318a8
bpross <bpross@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-10 04:32:52 +0000

    Added some functions
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@158 24d622c1-b139-487c-8c47-8fc189a5de26

--- algorithm/new_algorithm/algorithm/professor.py
+++ algorithm/new_algorithm/algorithm/professor.py
@@ -2,6 +2,7 @@
 #CMPS 115
 #Linda Werner
 #professor.py
+#Authors: egall24, bpross
 
 import sys
 
@@ -9,10 +10,33 @@ class Professor:
  	id = 0
         name = ''
         #initializes professor with ID and name
-	def __init__(self, id, name = None):
-		self.id = id
-                self.name = name
+	def __init__(self, id = None, name = None,course_list = None):
+		if id is None:
+			self.id = None
+		else:
+			self.id = id
+		if name is None:
+			self.name = None
+		else:
+			self.name = name
+		if course_list is None:
+			self.course_list = None
+		else:
+			sself.course_list = course_list
 
+
+	#Returns the professor's ID
+	def get_id(self):
+		return self.id
+
+	#Returns the professor's name
+	def get_name(self):
+		return self.name
+
+	#Returns the professor's course list
+	def get_courses(self):
+		return self.course_list
+	
 	#prints out the professors data
 	def print_professor(self):
 		print "(%d, %s)" % (self.id, self.name)
