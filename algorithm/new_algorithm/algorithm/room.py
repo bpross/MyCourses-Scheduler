7d7351bf2fd8a63c65f38c46fe7da31976723428
bpross <bpross@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-10 20:50:52 +0000

    Added manipulation methods
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@172 24d622c1-b139-487c-8c47-8fc189a5de26

--- algorithm/new_algorithm/algorithm/room.py
+++ algorithm/new_algorithm/algorithm/room.py
@@ -28,14 +28,26 @@ class Room:
 	def get_seat_num(self):
 		return self.seat_num
 
+	#Adds/changes the seat number of the room
+	def add_seats(self, seat_num):
+		self.seat_num = seat_num
+		
 	#Returns the Name of the Room
 	def get_name(self):
 		return self.name
 
+	#Adds/changes the name of the room
+	def add_name(self,name):
+		self.name = name
+		
 	#Returns True if Room has lab, False if no Lab
 	def lab_status(self):
 		return self.lab
-	
+
+	#Adds/changes the lab status of the room
+	def add_lab(self,lab):
+		self.lab = lab
+		
         #prints out the room data
         def print_room(self):
                 print "(%d, %d, %s)" % (self.seat_num, self.lab, self.name)
