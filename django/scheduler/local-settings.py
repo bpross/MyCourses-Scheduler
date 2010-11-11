10adeccd2b87438f2670836a7fdce1f6e74ffb31
whimsy <whimsy@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-11 01:01:22 +0000

    Ugh. What is this bullshit.
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@186 24d622c1-b139-487c-8c47-8fc189a5de26

--- django/scheduler/local-settings.py
+++
@@ -1,8 +0,0 @@
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-#
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://67.180.228.22/media/'
-
