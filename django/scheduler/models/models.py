fd6e0f5a8c7530d9f88ab6fda5259e06f0bd3b60
egall24 <egall24@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-16 22:32:23 +0000

    Not too much, just wanted to save
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@198 24d622c1-b139-487c-8c47-8fc189a5de26

--- django/scheduler/models/models.py
+++ django/scheduler/models/models.py
@@ -8,16 +8,25 @@ from django.db import models
 class Professor(models.Model):
 	id = models.IntegerField()
 	name = models.CharField(max_length=50)
+	
+	def __unicode__(self):
+		return self.name
 
 class Course(models.Model):
 	id = models.IntegerField()
 	name = models.CharField(max_length=50)
+	
+	def __unicode__(self):
+		return self.name
 
 class Room(models.Model):
 	seat_num = models.IntegerField()
 	lab = models.CharField(max_length=50)
 	name = models.CharField(max_length=50)
 	requires_lab = models.IntegerField()
+	
+	def __unicode__(self):
+		return self.name
 
 
 class Course_Class(models.Model):
@@ -26,3 +35,6 @@ class Course_Class(models.Model):
 	num_seats = models.IntegerField()
 	requires_lab = models.IntegerField()
 	duration = models.IntegerField()
+	
+	def __unicode__(self):
+		return self.name 
