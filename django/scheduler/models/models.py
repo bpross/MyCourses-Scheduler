e6c878e6c9e8ac51d23a920b088526f64d554cee
egall24 <egall24@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-11-17 06:11:53 +0000

    Not much done, getting code written out mostly
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@203 24d622c1-b139-487c-8c47-8fc189a5de26

--- django/scheduler/models/models.py
+++ django/scheduler/models/models.py
@@ -19,11 +19,16 @@ class Course(models.Model):
 	def __unicode__(self):
 		return self.name
 
+class Buildings(Course):
+	name = models.CharField(max_length=50)
+
+	def __unicode__(self):
+
 class Room(models.Model):
 	seat_num = models.IntegerField()
 	lab = models.CharField(max_length=50)
 	name = models.CharField(max_length=50)
-	requires_lab = models.IntegerField()
+	requires_lab = models.BooleanField()
 	
 	def __unicode__(self):
 		return self.name
@@ -33,8 +38,43 @@ class Course_Class(models.Model):
 	professor = models.ForeignKey(Professor)
 	course = models.ForeignKey(Course)
 	num_seats = models.IntegerField()
-	requires_lab = models.IntegerField()
+	requires_lab = models.BooleanField()
 	duration = models.IntegerField()
-	
+
+class Master_table(models.Model):
+	professor = models.ManyToMany(Professor)
+	room = models.ManyToMany(Room)
+	course = models.ManyToMany(Course)
 	def __unicode__(self):
 		return self.name 
+	
+class Person(models.Model):
+	first_name = models.CharField(max_lenght=50)
+	minitial = models.CharField(max_length=1)
+	last_name = models.CharField(max_length=50)
+	suffix = models.CharField(max_length=50)
+	prefix = modles.CharField(max_length=50)
+	def __unicode__(self):
+		return self.name 
+	
+
+class School(models.Model):
+    school = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name 
+	
+class Department(models.Model):
+	department = modles.CharField(max_lenght=50)
+	dep_abbrev = models.CharField(max=length=4)
+	chair_id = models.ForeignKey(Chair)
+	school_id = models.ForeignKey(School)
+
+    
+	
+
+
+	
+
+
+
+
