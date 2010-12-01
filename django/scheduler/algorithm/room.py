19bf28678429c17479e63f3d802f86295a58f3fa
egall24 <egall24@24d622c1-b139-487c-8c47-8fc189a5de26>
2010-12-01 07:39:06 +0000

    added rooms.py
    git-svn-id: http://gforge.soe.ucsc.edu/svn/scheduling115/trunk@282 24d622c1-b139-487c-8c47-8fc189a5de26

---
+++ django/scheduler/algorithm/room.py
@@ -0,0 +1,67 @@
+#Course Scheduling Algorithm
+#CMPS 115
+#Linda Werner
+#room.py
+#Author: egall24, bpross
+
+import sys
+
+class Rooms:
+	
+        #initializes room with ID and name
+        def __init__(self, seat_num = None, lab = None, name = None):
+		if seat_num is None:
+			self.seat_num = None
+		else:
+			self.seat_num = seat_num
+		if lab is None:
+			self.lab = None
+		else:
+			self.lab = lab
+		if name is None:
+			self.name = None
+		else:
+			self.name = name
+
+		self.id = None
+
+	#Returns the ID of the room
+	def get_id(self):
+		return self.id
+	
+	#Returns the seat number of the room
+	def get_seat_num(self):
+		return self.seat_num
+
+	#Adds/changes the seat number of the room
+	def add_seats(self, seat_num):
+		self.seat_num = seat_num
+		
+	#Returns the Name of the Room
+	def get_name(self):
+		return self.name
+
+	#Adds/changes the name of the room
+	def add_name(self,name):
+		self.name = name
+		
+	#Returns True if Room has lab, False if no Lab
+	def lab_status(self):
+		return self.lab
+
+	#Adds/changes the lab status of the room
+	def add_lab(self,lab):
+		self.lab = lab
+		
+        #prints out the room data
+        def print_room(self):
+                print "(%d, %d, %s)" % (self.seat_num, self.lab, self.name)
+
+
+if __name__ == '__main__':
+
+        p1 = Rooms(50, True, "BE105")
+        p1.print_room()
+        sys.exit(0)
+
+	
