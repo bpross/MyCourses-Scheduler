219071fa6c6e22984416ab1e482cbbdd41fdb417
Sab Pets <sabbapetri@sabba-petris-computer.local>
2011-02-07 19:44:22 -0800

    admin updated with mass database editing

--- django/scheduler/algorithm/admin.py
+++ django/scheduler/algorithm/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from scheduler.algorithm.models import *
-admin.site.register(School)
-admin.site.register(Department)
-admin.site.register(Class)
-admin.site.register(Prerequisite)
-admin.site.register(Building)
-admin.site.register(Room)
-admin.site.register(Period)
-admin.site.register(Lecturer)
-admin.site.register(ClassInstance)
-admin.site.register(ClassLab)
-admin.site.register(Person)
-admin.site.register(Role)
-admin.site.register(PersonRole)
+
+admin.site.register(School, SchoolAdmin)
+admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Prerequisite, PrerequisiteAdmin)
+admin.site.register(Building, BuildingAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Period, PeriodAdmin)
+admin.site.register(Lecturer, LecturerAdmin)
+admin.site.register(ClassInstance, ClassInstanceAdmin)
+admin.site.register(ClassLab, ClassLabAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Role, RoleAdmin)
+admin.site.register(PersonRole, PersonRoleAdmin)
+
+
