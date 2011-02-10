2a809d25f223f8bc7a2728922b53ec3cf86a9d75
Will Crawford <wacrawfo@ucsc.edu>
2011-02-09 23:00:14 -0800

    I think I fixed it...

--- django/scheduler/administrator/urls.py
+++ django/scheduler/administrator/urls.py
@@ -4,5 +4,6 @@ from scheduler.administrator.views import *
 urlpatterns = patterns('administrator.views',
     (r'^$', 'home' ),
     (r'^home/', 'home' ),
-	 (r'^import/', 'upload_csv'),
+	(r'^import/', 'upload_csv'),
+	(r'^csv/', 'upload_csv'),
 )
