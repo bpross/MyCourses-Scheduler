55c23a0a098fbdcca39be7d4dd96bfef5b26f2f5
Will Crawford <wacrawfo@ucsc.edu>
2011-03-02 11:41:56 -0800

    Registered the new models in the admin.py

--- django/scheduler/algorithm/admin.py
+++ django/scheduler/algorithm/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 from scheduler.algorithm.models import *
 
-admin.site.register(School, SchoolAdmin)
-admin.site.register(Department, DepartmentAdmin)
-admin.site.register(Class, ClassAdmin)
-admin.site.register(Prerequisite, PrerequisiteAdmin)
+admin.site.register(Business, BusinessAdmin)
 admin.site.register(Building, BuildingAdmin)
-admin.site.register(Room, RoomAdmin)
-admin.site.register(Period, PeriodAdmin)
-admin.site.register(Lecturer, LecturerAdmin)
-admin.site.register(ClassInstance, ClassInstanceAdmin)
-admin.site.register(ClassLab, ClassLabAdmin)
-admin.site.register(Person, PersonAdmin)
-admin.site.register(Role, RoleAdmin)
-admin.site.register(PersonRole, PersonRoleAdmin)
+admin.site.register(Employer, EmployerAdmin)
+admin.site.register(Employee)
 
 
