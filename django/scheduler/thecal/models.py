924fc021789156be6814b99febc821e06330ef89
Erik Steggall <esteggall@MacBook-58.local>
2011-03-07 21:27:08 -0800

    Added fields to thecal.models.py

--- django/scheduler/thecal/models.py
+++ django/scheduler/thecal/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# Calendar
+class Calendar(models.Model):
+    idCal = models.IntegerField(null=True)
+    title = models.CharField(max_length=45))
+    start = models.DateTimeField(auto_now=False, auto_now_add=False)
+    end   = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __unicode__(self):
+        return self.idCal
