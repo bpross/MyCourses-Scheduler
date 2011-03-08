2a6d37941f3ce4d8dcea07b940fe027573e262f4
Will Crawford <wacrawfo@ucsc.edu>
2011-03-07 21:34:45 -0800

    Removed some garbage

--- django/scheduler/thecal/views.py
+++ django/scheduler/thecal/views.py
@@ -2,25 +2,6 @@ from django.http import HttpResponseRedirect, HttpResponse, HttpResponseServerEr
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-'''
-def upload_csv(request):
-	if request.method == 'POST':
-		form = csv_form(request.POST, request.FILES)
-		if form.is_valid():
-			myCSV = CSV()
-			print "File type selected: %s" %(request.POST['type'])
-			result = myCSV.csv_import(request.FILES['file'], request.POST['type'])
-			if result:
-				flag = "successful"
-			else:
-				flag = "failed"
-			html = "<html><body>Upload %s.</body></html>" % (flag)
-			return HttpResponse(html)
-	else:
-		form = csv_form()
-	return render_to_response('admin/csv_form.html', {'form': form}, RequestContext(request))
-	
-'''
 
 def calendar(request):
 	return render_to_response('calendar.html', RequestContext(request))
