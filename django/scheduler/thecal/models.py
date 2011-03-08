dff8f8391e8555d3686356e330d129419f7b96d8
Will Crawford <wacrawfo@ucsc.edu>
2011-03-07 21:42:14 -0800

    thecal/models updated again

--- django/scheduler/thecal/models.py
+++ django/scheduler/thecal/models.py
@@ -3,11 +3,20 @@ from django.db import models
 # Create your models here.
 
 # Calendar
-class Shift(models.Model):
+class inputShift(models.Model):
     idCal = models.IntegerField(null=True)
     title = models.CharField(max_length=45)
     start = models.DateTimeField(auto_now=False, auto_now_add=False)
     end   = models.DateTimeField(auto_now=False, auto_now_add=False)
+    # positions necessary?
+        def __unicode__(self):
+        return self.idCal
+
+class outputShift(models.Model):
+	# Start
+	# End
+	# Title
+	# People on this shift
 
 class Week(models.Model):
 	# one to many key to a bunch of shifts.
@@ -15,6 +24,3 @@ class Week(models.Model):
 class Month(models.Model):
 	# A month should own several weeks?
 	# But what about weeks that fall across months?
-
-    def __unicode__(self):
-        return self.idCal
