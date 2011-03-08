1b6c4357fa2ade4c8273ca05d446c9db88897d73
Will Crawford <wacrawfo@ucsc.edu>
2011-03-08 14:34:45 -0800

    Models for thecal updated a bit.

--- django/scheduler/thecal/models.py
+++ django/scheduler/thecal/models.py
@@ -7,7 +7,11 @@ import scheduler.algorithm.models as algomodels
 class Person(models.Model):
     business = models.ForeignKey(algomodels.Business)
     name = models.CharField(primary_key=True, max_length=45)
+    # This should be a foreign key to an employee field, shouldn't it?
+    # Otherwise we're duplicating data.
     position = models.CharField(max_length=45)
+    # This should be a foreign key to an employee field, shouldn't it?
+    # Otherwise we're duplicating data.
     def __unicode__(self):
         return self.name
 
@@ -17,7 +21,9 @@ class inputShift(models.Model):
     business = models.ForeignKey(algomodels.Business)
     start = models.DateTimeField(auto_now_add=False)
     end   = models.DateTimeField(auto_now=False, auto_now_add=False)
-    people = models.ManyToManyField(Person) 
+    #people = models.ManyToManyField(Person) 
+    # We don't need this until we enable manual assignment
+    # on scheduler input.
     position = models.CharField(max_length=45)
     def __unicode__(self):
         return self.title
@@ -34,19 +40,32 @@ class outputShift(models.Model):
         return self.title
 
 # Day
-class Day(models.Model):
+class inputDay(models.Model):
+    name = models.CharField(primary_key=True, max_length=45)
+    business = models.ForeignKey(algomodels.Business)
+    shifts = models.ManyToManyField(inputShift)
+    def __unicode__(self):
+        return self.name
+        
+class outputDay(models.Model):
     name = models.CharField(primary_key=True, max_length=45)
     business = models.ForeignKey(algomodels.Business)
     shifts = models.ManyToManyField(outputShift)
     def __unicode__(self):
         return self.name
 
+
 # Week
-class Week(models.Model):
+class inputWeek(models.Model):
     business = models.ForeignKey(algomodels.Business)
     start = models.DateTimeField(auto_now_add=False)
     end   = models.DateTimeField(auto_now=False, auto_now_add=False)
-    days  = models.ManyToManyField(Day)
+    days  = models.ManyToManyField(inputDay)
 
+class outputWeek(models.Model):
+    business = models.ForeignKey(algomodels.Business)
+    start = models.DateTimeField(auto_now_add=False)
+    end   = models.DateTimeField(auto_now=False, auto_now_add=False)
+    days  = models.ManyToManyField(outputDay)
     
     
